5d8052a3dc17df077324d8a76a52e6de6710c8b7
jackskii <87918103+jackskii@users.noreply.github.com>
2022-11-04 23:00:03 +0800

    Add files via upload

---
+++ mining.py
@@ -0,0 +1,49 @@
+import hashlib
+import json
+from datetime import datetime
+
+#init difficulty
+difficulty = 5
+#previous block time
+prev_time = datetime.now()
+
+class block:
+    def __init__(self, seq_no=0, prev_hash=None, data=None):
+        self.seq_no = seq_no
+        self.prev_hash = prev_hash
+        self.data = data
+        self.timestamp = datetime.now()
+
+
+    #finds the hash of a block
+    def nonce_hash(nonce, block):
+        global difficulty
+        global prev_time
+        print(prev_time)
+        for i in range(10000000):
+            hash1 = hashlib.md5(str(block).encode('utf-8')).hexdigest()
+            hash2 = hashlib.md5((str(nonce) + str(hash1)).encode('utf-8')).hexdigest()
+            comp = ['0'] * difficulty
+            if (hash2[:difficulty] == ''.join(comp)):
+                print('hash2', hash2)
+                print('yes')
+                break
+            else:
+                nonce += 1
+        this_time = datetime.now()
+        #change difficulty
+        delta = this_time - prev_time
+        print(difficulty, delta.total_seconds())
+        if (delta.total_seconds() < 5):
+            difficulty += 1
+        if (delta.total_seconds() > 15):
+            difficulty -= 1
+        return hash2
+
+def main():
+    #b tries to mine block
+    b = block(data='abdasfd')
+    hash = block.nonce_hash(1, b)
+
+if __name__ == "__main__":
+    main()
