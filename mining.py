3706288505c18736076414caf81a748bd07ac298
Qing Hou <qhou822@gmail.com>
2022-11-05 16:06:38 +0800

    tested adding 10 blocks

--- mining.py
+++ mining.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
 from datetime import datetime
+import random
+
 
 class block(object):
     def __init__(self, index, transactions, timestamp, previous_hash, current_hash, difficulty, proof):
@@ -13,65 +15,88 @@ class block(object):
         self.proof = proof
 
     def change_difficulty(self, delta):
-        if (delta.total_seconds() < 5):
-            self.difficulty += 1
+        if (delta.total_seconds() < 15):
+            self.difficulty += 0
         if (delta.total_seconds() > 15):
-            self.difficulty -= 1
+            self.difficulty -= 0
 
     #get hash of current block using other data of block
-    def hash_block(self, transactions, index, timestamp, previous_hash, proof):
-        t = hashlib.sha256((''.join(transactions)).encode('utf-8')).hexdigest()
-        i = hashlib.sha256(str(index).encode('utf-8')).hexdigest()
-        ts = hashlib.sha256(timestamp.strftime("%m%d%Y%H%M%S").encode('utf-8')).hexdigest()
-        ph = hashlib.sha256(previous_hash.encode('utf-8')).hexdigest()
-        p = hashlib.sha256(str(proof).encode('utf-8')).hexdigest()
-        final_hash = hashlib.sha256((t+i+ts+ph+p).encode('utf-8')).hexdigest()
+    def hash_block(self):
+        t = hashlib.sha256((''.join(map(str, self.transactions))).encode('utf-8')).hexdigest()
+        i = hashlib.sha256(str(self.index).encode('utf-8')).hexdigest()
+        ts = hashlib.sha256(self.timestamp.strftime("%m%d%Y%H%M%S").encode('utf-8')).hexdigest()
+        ph = hashlib.sha256(str(self.previous_hash).encode('utf-8')).hexdigest()
+        d = hashlib.sha256(str(self.difficulty).encode('utf-8')).hexdigest()
+        p = hashlib.sha256(str(self.proof).encode('utf-8')).hexdigest()
+        final_hash = hashlib.sha256((t+i+ts+ph+d+p).encode('utf-8')).hexdigest()
         return final_hash
     
-    #get hash of block+nonce
-    def hash_nonce(self):
-        return hashlib.sha256((str(self.proof)+self.current_hash).encode('utf-8')).hexdigest()
 
     #mine a block
     def mine(self):
         difficulty = self.difficulty
-        prev_time = datetime.now()
-        hash1 = hashlib.sha256(str(block).encode('utf-8')).hexdigest()
+        start_time = datetime.now()
+        hash1 = self.hash_block()
         while True:
-            hash2 = self.hash_nonce()
+            hash2 = hashlib.sha256((str(self.proof)+str(hash1)).encode('utf-8')).hexdigest()
             comp = ['0'] * difficulty
             if (hash2[:difficulty] == ''.join(comp)):
-                print('hash2', hash2)
                 break
             else:
                 self.proof += 1
         this_time = datetime.now()
         #change difficulty
-        delta = this_time - prev_time
-        print('difficulty:', difficulty, '  time took:', delta.total_seconds())
+        delta = this_time - start_time
+        #print('difficulty:', difficulty, '  time took:', delta.total_seconds())
         self.change_difficulty(delta)
-        return hash2,difficulty
+        return hash2
 
+#print block nicely
+def print_block(b):
+    print("block nubmer: %s\n  timestamp: %s\n  previous_hash: %s\n  hash: %s\n  difficulty: %s\n  nonce: %s\n  transactions: %s\n" %
+            (str(b.index),
+            str(b.timestamp.strftime("%H:%M:%S")),
+            str(b.previous_hash),
+            str(b.current_hash),
+            str(b.difficulty),
+            str(b.proof),
+            str("\n                ".join(map(str, b.transactions))))
+            )
 def main():
+    #hard code blokchain info for now
+    chain = []
     #b tries to mine block
     b = block(index=0, 
-                transactions='a', 
+                transactions=['OwO','UwU','VwV'], 
                 timestamp=datetime.now(), 
                 previous_hash="", 
                 current_hash=None, 
                 difficulty=5, 
                 proof=0)
-    b.current_hash = b.hash_block(b.transactions, b.index, b.timestamp, b.previous_hash, b.proof)
-    hash,dif = b.mine()
-    #adds the mined block to chain
-    b2 = block(index=b.index+1,
-                transactions=['OwO','UwU','VwV'],
-                timestamp=datetime.now(),
-                previous_hash=hash,
-                current_hash=None,
-                difficulty = dif,
-                proof=0)
-    print(b2.previous_hash)
+    b.current_hash = b.hash_block()
+    #hard coding
+    chain.append(b)
+    print_block(b)
+    #loop to mine 10 blocks
+    n = 10
+    a = [0] * n
+    #prev_hash init as hash of first block
+    prev_hash = b.current_hash
+    for i in range(10):
+        #get some random data
+        tt = [random.random()]
+        temp = block(
+            index = len(chain),
+            transactions=tt, 
+            timestamp=datetime.now(), 
+            previous_hash=chain[-1].current_hash, 
+            current_hash='',
+            difficulty=chain[-1].difficulty, 
+            proof=chain[-1].proof
+        )
+        temp.current_hash=temp.mine()
+        print_block(temp)
+        chain.append(temp)
 
 if __name__ == "__main__":
     main()
